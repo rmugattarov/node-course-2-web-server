2c4fed2a30998088c5a43b766fc0012ad48d7ad6
Robert Mugattarov <mugattarov@gmail.com>
2018-11-22 12:08:49 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view-engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  fs.appendFile('server.log', `${now}: ${req.method} ${req.url}\n`, (e) => {
+    console.log(e);
+  });
+  next();
+});
+app.use((req, res, next) => {
+  res.render('maintenance.hbs', {});
+  next();
+});
+app.use(express.static(__dirname + '/static'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (s) => {
+  return s.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    homePageText: "D'Oh!"
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000')
+});
